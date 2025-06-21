b2acf0cb1c393158fd116fde40f12379369d6a01
Nathan-Kimm <nathanjkim2007@gmail.com>
2025-06-21 11:43:54 -0400

    Initial Commit

---
+++ Chapter1/1.2/TemperatureConverter/temperature.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    //While Loop Version
+    int fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    fahr = lower;
+    while(fahr <= upper) {
+        celsius = (5.0/9.0) * (fahr - 32);
+        printf("%d\t%d\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+
+    //For Loop Version
+    int fahr, celsius;
+    int upper = 300;
+
+    for(fahr = 0; fahr <= upper; fahr = fahr + 20) {
+        celsius = (5.0/9.0) * (fahr-32);
+        printf("%d\t%d\n", fahr, celsius);
+    }
+}
